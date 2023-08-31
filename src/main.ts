a3e2901940a7402e873de95d0ad29487911fa2c5
황상진 <aosdb5640@naver.com>
2023-09-01 04:25:04 +0900

    add: cors setting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exceptions/http.exceptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
