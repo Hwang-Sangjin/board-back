91d99e513b593963a2946c3e65ac04174f3ec35b
황상진 <aosdb5640@naver.com>
2023-08-31 23:34:49 +0900

    add: jwt token

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -12,7 +12,7 @@ export class User {
   username: string;
 
   @ApiProperty({ description: '유저 비밀번호' })
-  @Column({ select: false })
+  @Column()
   password: string;
 
   @ApiProperty({ description: '이름' })
--- src/routes/user/user.service.ts
+++ src/routes/user/user.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { hash, compare } from 'bcrypt';
 import { Board } from 'src/entity/board.entity';
 import { User } from 'src/entity/user.entity';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
-import { hash } from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class UserService {
@@ -14,7 +16,7 @@ export class UserService {
   ) {}
 
   async createUser(data: CreateUserDto) {
-    const { username, password, name } = data;
+    const { username, name, password } = data;
 
     const encryptedPassword = await this.encryptPassword(password);
 
@@ -23,6 +25,40 @@ export class UserService {
       name,
       password: encryptedPassword,
     });
+  }
+
+  async getUserByUsername(username: string) {
+    return this.userRepository.findOneBy({
+      username,
+    });
+  }
+
+  async login(data: LoginUserDto) {
+    const { username, password } = data;
+
+    const user = await this.userRepository.findOneBy({
+      username,
+    });
+
+    if (!user) throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+
+    const match = await compare(password, user.password);
+
+    if (!match)
+      throw new HttpException('UNAUTHORIZED', HttpStatus.UNAUTHORIZED);
+
+    const payload = {
+      username,
+      name: user.name,
+    };
+
+    const accessToken = jwt.sign(payload, 'secret_key', {
+      expiresIn: '1h',
+    });
+
+    return {
+      accessToken,
+    };
   }
 
   async getUser() {
